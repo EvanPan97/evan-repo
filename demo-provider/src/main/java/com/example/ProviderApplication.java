01f4ad710100e485632358c2812f4e04a174f577
Evan.Pan <875426378@qq.com>
2021-10-02 17:44:24 +0800

    add: demo-provider配置主启动类, 添加nacos-discovery依赖, 新增dev配置文件

---
+++ demo-provider/src/main/java/com/example/ProviderApplication.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ProviderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApplication.class, args);
+    }
+
+    @RestController
+    public class ProviderController {
+        @Value("${server.port:8080}")
+        private String server;
+
+        /** url = http://localhost:8081/provider/echo/{msg}
+         * @param msg
+         * @return "localhost:8081 say {msg}"
+         */
+        @GetMapping("/provider/echo/{msg}")
+        public String doRestDefaultEcho(@PathVariable String msg) {
+            return server + " say " + msg;
+        }
+    }
+}
